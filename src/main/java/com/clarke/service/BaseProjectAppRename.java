06686833e1a037871b5280a975f972d86bed7356
Clarke <clarke@HP-Laptop>
2024-09-24 09:02:58 -0400

    First Time

---
+++ src/main/java/com/clarke/service/BaseProjectAppRename.java
@@ -0,0 +1,13 @@
+package com.clarke.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProjectAppRename {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseProjectAppRename.class, args);
+    }
+
+}
